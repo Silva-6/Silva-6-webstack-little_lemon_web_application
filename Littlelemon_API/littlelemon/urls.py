43777b6155e37b761a4bb2b0e2ee35ad6d3d6c97
Silva-6 <silva6media@gmail.com>
2024-06-02 20:23:19 +0100

    Add the routers.urls to URLConf.

--- Littlelemon_API/littlelemon/urls.py
+++ Littlelemon_API/littlelemon/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant import views
 
+
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 urlpatterns = [
